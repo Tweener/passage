27c17be260c3ead31c018d9b18c4184a9ffe7afd
Vivien Mahé <vivouneuh@gmail.com>
2024-11-29 14:52:48 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kmplate_Library"
+include(":changehere")
