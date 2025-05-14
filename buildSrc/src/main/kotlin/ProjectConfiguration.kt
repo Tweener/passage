37beba0f63056d686a6e99d4868f7cf2a070d507
Vivien Mahé <vivouneuh@gmail.com>
2025-05-14 14:56:46 +0200

    Bump Passage to version 1.4.0

--- buildSrc/src/main/kotlin/ProjectConfiguration.kt
+++ buildSrc/src/main/kotlin/ProjectConfiguration.kt
@@ -9,7 +9,7 @@ object ProjectConfiguration {
 
     object Passage {
         const val packageName = "com.tweener.passage"
-        const val versionName = "1.3.5"
+        const val versionName = "1.4.0"
         const val namespace = "$packageName.android"
         const val compileSDK = 35
         const val minSDK = 24
